0a80f9ebc83ddcce72b74dca2ef1d2c245f054cb
Steffen Tröster <stetro@p219196.vpn-dyn.fh-koeln.de>
2012-11-07 10:25:34 +0100

    TimeLineController Interaktion funktioniert

---
+++ src/main/java/de/fhkoeln/eis/radioexpert/client/TimeLineController.java
@@ -0,0 +1,11 @@
+package de.fhkoeln.eis.radioexpert.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Steffen Tröster
+ * Date: 07.11.12
+ * Time: 09:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class TimeLineController {
+}
