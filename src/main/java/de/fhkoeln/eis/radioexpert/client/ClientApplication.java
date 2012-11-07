24c0096eb358149acceecef780a3b96d03123496
Steffen Tröster <stetro@p219196.vpn-dyn.fh-koeln.de>
2012-11-07 10:25:59 +0100

    TimeLineController Interaktion funktioniert

--- src/main/java/de/fhkoeln/eis/radioexpert/client/ClientApplication.java
+++ src/main/java/de/fhkoeln/eis/radioexpert/client/ClientApplication.java
@@ -1,19 +1,25 @@
 package de.fhkoeln.eis.radioexpert.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class ClientApplication extends Application {
+
+    @FXML
+    Line timeLineLine;
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/form.fxml"));
         stage.setTitle("FXML Welcome");
-        stage.setScene(new Scene(root, 300, 275));
+        stage.setScene(new Scene(root, 600, 400));
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/de/fhkoeln/eis/radioexpert/client/TimeLineController.java
+++ src/main/java/de/fhkoeln/eis/radioexpert/client/TimeLineController.java
@@ -1,5 +1,14 @@
 package de.fhkoeln.eis.radioexpert.client;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Steffen Tröster
@@ -7,5 +16,40 @@ package de.fhkoeln.eis.radioexpert.client;
  * Time: 09:58
  * To change this template use File | Settings | File Templates.
  */
-public class TimeLineController {
+public class TimeLineController implements Initializable {
+
+    @FXML
+    Line timeLineLine;
+    @FXML
+    Ellipse timeLineEllipse;
+
+    @FXML
+    ListView listListView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.print(timeLineLine.toString());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        timeLineLine.setStartY(timeLineLine.getStartY() + 1);
+                        timeLineLine.setEndY(timeLineLine.getEndY() + 1);
+
+                        timeLineEllipse.setCenterY(timeLineEllipse.getCenterY() + 1);
+
+                        synchronized (listListView.getItems()) {
+                            listListView.getItems().add("Fooo");
+                        }
+
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        break;
+                    }
+                }
+            }
+        }).start();
+    }
 }
